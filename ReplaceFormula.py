f33115ff4be630c0275920a0673464d7a851fd0d
rherrma1 <rherrma1@gmail.com>
2020-05-13 11:03:41 -0400

    updated replace formula
    Converts mathematica output into a format Couenne can read

---
+++ ReplaceFormula.py
@@ -0,0 +1,30 @@
+import re
+
+# change us!
+inputFile = "P5ExpectedValue.txt"
+outputFile = "P5ExpectedValueresult.txt"
+
+#--------------------------------------------------------------
+
+# prepend to file
+ofo = open(outputFile, "w")
+ofo.write("var s >= 0, <= 7;\n")
+ofo.write("var t >= 0, <= 7;\n")
+ofo.write("maximize obj: ")
+
+# read, process and write to file
+ifo = open(inputFile, "r")
+data = ifo.read()
+data = data.replace("Sin", "sin")
+data = data.replace("Cos", "cos")
+data = data.replace("[", "(")
+data = data.replace("]", ")")
+data = data.replace("{", "(")
+data = data.replace("}", ")")
+data = re.sub(r'([0-9\)]) ([a-zA-Z\(])', r'\1*\2', data)
+data = re.sub(r'([\+\-])', r' \1 ', data)
+ofo.write(data+";")
+
+# close files
+ifo.close()
+ofo.close()
